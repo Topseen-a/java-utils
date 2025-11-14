57dc138590a739fa7b12944bf6512952f2c1e3c5
Topseen-a <abodunrintopseen@gmail.com>
2025-11-14 08:25:53 +0100

    initial commit

---
+++ chapter4/GasMileage.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+    public class GasMileage{
+        public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter number of miles driven: ");
+        int milesDriven = input.nextInt();
+
+        System.out.print("Enter the number of gallons used: ");
+        int gallonUsed = input.nextInt();
+
+
+
+        }
+
+
+
+
+    }
