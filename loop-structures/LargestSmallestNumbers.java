ba1167aaed5c59a9e119897a1df6d813c25eab77
Topseen-a <abodunrintopseen@gmail.com>
2025-11-17 23:18:24 +0100

    loop structure

--- loop-structures/LargestSmallestNumbers.java
+++ loop-structures/LargestSmallestNumbers.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+    public class LargestSmallestNumbers{
+        public static void main(String[] args){
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int largest = number;
+        int smallest = number;
+
+        while(true){
+            
+            System.out.print("Enter the numbers you want: ");
+            number = input.nextInt();
+
+            if(number == -1) {
+                break;
+            }
+            if(number > largest) {
+            largest = number;
+            }
+            if (number < smallest) {
+            smallest = number;
+            }
+        }
+
+        System.out.println("Largest number is: " + largest);
+        System.out.println("Smallest number is: " + smallest);
+    }
+}
