1f54a3351a2bbbbbe7076dded50715b2d2dc7459
Topseen-a <abodunrintopseen@gmail.com>
2025-11-17 22:03:20 +0100

    chapter 4

--- chapter4/GasMileage.java
+++ chapter4/GasMileage.java
@@ -3,18 +3,31 @@ import java.util.Scanner;
         public static void main(String[] args){
 
         Scanner input = new Scanner(System.in);
+
+        int miles = 0;
+        int gallon = 0;
+
+        while(true){
         
-        System.out.print("Enter number of miles driven: ");
-        int milesDriven = input.nextInt();
+            System.out.print("Enter number of miles driven or -1 to exit: ");
+            int milesDriven = input.nextInt();
 
-        System.out.print("Enter the number of gallons used: ");
-        int gallonUsed = input.nextInt();
+            if(milesDriven == -1){
+                System.out.println("Exiting program");
+                break;     
+                }
 
+                System.out.print("Enter the number of gallons used: ");
+                int gallonUsed = input.nextInt();
 
+                double trip = milesDriven / gallonUsed;
+                System.out.println("Miles per gallon is: ", + trip);
+                
+                miles = miles + milesDriven;
+                gallon = gallon + gallonUsed;
 
-        }
-
-
-
-
+                double totalMilesPerGallon = miles / gallon;
+                System.out.println("Miles per gallon is: " + totalMilesPerGallon);
+                }
+            }
     }
