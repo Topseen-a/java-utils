8d7197abad04e54de2905ee61ae06fc0afa4ece3
Topseen-a <abodunrintopseen@gmail.com>
2025-11-22 21:29:07 +0100

    java statement

---
+++ chapter4/Statement.java
@@ -0,0 +1,10 @@
+public class Statement{
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Before: x = " + x);
+
+        x += x++ - 5;
+
+        System.out.println("After: x = " + x);
+    }
+}
