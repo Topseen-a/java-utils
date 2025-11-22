a33f7bd8d409fb6d0868412c6d95b144bfb72b33
Topseen-a <abodunrintopseen@gmail.com>
2025-11-22 20:20:20 +0100

    loop

---
+++ chapter4/Palindrome.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+    public class Palindrome{
+        public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a five digit integer: ");
+        int number = input.nextInt();
+    while(true){
+        if (number >= 10000 && number <= 99999){
+            break;
+            }
+        else {
+            System.out.println("Invalid input, enter a five digit integer: ");
+            }
+         }
+            int numberOne = number / 10000;
+            int numberTwo = (number / 1000) % 10;
+            int numberThree = (number / 100) % 10;
+            int numberFour = (number / 10) % 10;
+            int numberFive = number % 10;
+
+        if (numberOne == numberFive && numberTwo == numberFour){
+            System.out.println("It is a palindrome");
+            }
+        else {
+            System.out.println("It is not a palindrome");
+            }
+        }
+
+    }
