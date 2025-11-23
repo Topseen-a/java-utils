d677568215f80bb385c54dc9ab448b664eb45daa
Topseen-a <abodunrintopseen@gmail.com>
2025-11-23 19:47:50 +0100

    java

---
+++ javafunctions/Kata.java
@@ -0,0 +1,17 @@
+public class Kata {
+    
+    public static boolean isEven(int number) {
+        if (number % 2 == 0){
+            return true;
+        } else {
+            return false;
+        }
+    public static boolean isPrime(int number) {
+        
+
+
+
+        }
+
+    }
+}
--- loop-structures/PrimeNumber.java
+++ loop-structures/PrimeNumber.java
@@ -5,14 +5,27 @@ import java.util.Scanner;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter a positive integer: ");
-        int number = input.nextInt();
+        int positiveNumber = input.nextInt();
+        int flag = 0;
 
-        if (number % 2 != 0) {
-            System.out.println(number + " is a prime number");
+    if (positiveNumber <= 1){
+        System.out.print("This number is not a prime number");
             }
-        else{
-            System.out.println(number + " is not a prime number");
+    else {
+        for (int count = 2; count <= positiveNumber - 1; count++){
+            if (positiveNumber % count == 0){
+                flag = 1; 
+                    break;            
+                    }
+                }
             }
+    if(flag == 0) {
+        System.out.println(positiveNumber + " is a prime number");    
+    } else {
+        System.out.println(positiveNumber + " is not a prime number");    
+    }
+
+
 
         }
 
