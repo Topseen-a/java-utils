ea43a8c7adc07970feb2103dbc6731bbea048c26
Topseen-a <abodunrintopseen@gmail.com>
2025-12-03 23:10:21 +0100

    task

---
+++ class-work/Table.java
@@ -0,0 +1,13 @@
+public class Table{
+    public static void main(String[] args){
+
+    System.out.println("N\tN2\tN3\tN4");
+
+    for (int count = 1; count <= 5; count++){
+        int countTwo = count * count;
+        int countThree = count * count * count;
+        int countFour = count * count * count * count;
+    System.out.println(count + "\t" + countTwo + "\t" + countThree + "\t" + countFour);
+     }
+    }
+}
