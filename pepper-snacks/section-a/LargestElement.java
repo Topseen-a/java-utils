177247a9bf22181bc166c2b0b73316a3937e264f
Topseen-a <abodunrintopseen@gmail.com>
2025-12-04 09:46:32 +0100

    pepper-snacks

---
+++ pepper-snacks/section-a/LargestElement.java
@@ -0,0 +1,32 @@
+public class LargestElement{
+    public static void main(String[] args){
+
+    System.out.println(isLargest([12,2,4,56,7]));
+    }
+
+public static int islargest(int number[]){
+//    number = number[];
+    int largest = number[0];
+
+    for (int count = 1, count <= number; count ++){
+        if (count > largest){
+            largest = count;
+        return largest;
+            }        
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
