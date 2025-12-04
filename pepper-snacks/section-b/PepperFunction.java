dc727988738ac545529b88d3e974fdf5ded485e2
Topseen-a <abodunrintopseen@gmail.com>
2025-12-04 21:08:22 +0100

    pepper-snacks

--- pepper-snacks/section-b/PepperFunction.java
+++ pepper-snacks/section-b/PepperFunction.java
@@ -3,14 +3,18 @@ public class PepperFunction{
     public static void main(String[] args){
 
     int[] array = {12,2,4,56,7};
-    int number = 12;
-    int result = isLargest(array);
-    System.out.println("The largest is " + result);
-    System.out.println(checkElement(array, number));
-    int result = getTotal(array);
-    System.out.println("The result is " + result);
-    System.out.println(Arrays.toString(getReverse(array)));
-    System.out.println(takeNumber(2342));  
+    int number = 56;
+//    int result = isLargest(array);
+//    System.out.println("The largest is " + result);
+//    System.out.println(checkElement(array, number));
+//    int result = getTotal(array);
+//    System.out.println("The result is " + result);
+//    System.out.println(Arrays.toString(getReverse(array)));
+//    System.out.println(takeNumber(2342));  
+//    int[] secondArray = {3,21,78,45,53,34};
+//    int[] firstArray = {32,215,80,71,89,04,78,90,43,21,89};
+//    System.out.println(Arrays.toString(combineArray(firstArray, secondArray)));
+    System.out.println(checkIndex(array, number));
     }
 
 public static int isLargest(int[] array){
@@ -51,6 +55,33 @@ public static int getTotal(int[] array){
     return sum;
     }
 
+public static int[] combineArray(int[] firstArray, int[] secondArray){
+    int[] newArray = new int [firstArray.length + secondArray.length];
+
+    int firstArrayCount = 0;
+    int secondArrayCount = 0;
+
+    for (int count = 0; count < newArray.length; count++){
+
+    if(firstArrayCount < firstArray.length){
+        newArray[count++] = firstArray[firstArrayCount];
+        firstArrayCount++;
+        }
+    if (secondArrayCount < secondArray.length){
+        newArray[count] = secondArray[secondArrayCount];
+        secondArrayCount++;
+        }
+    }
+    return newArray;
+    }
+
+public static int checkIndex(int[] array, int number){
+    for (int count = 0; count < array.length; count++){    
+        if (number == array[count])
+            return count;
+        }
+            return -1; 
+    }
 //public static int isOddPosition(int[] number){
 //    int[] odd = new int[size];
 //
