0a126279a84ece95e9959e9668e458cb302394f8
Topseen-a <abodunrintopseen@gmail.com>
2025-12-13 21:15:46 +0100

    chapter two

--- chapter2/StatementDisplay.java
+++ chapter2/StatementDisplay.java
@@ -8,5 +8,5 @@
         System.out.printf("Value of %d %d is %d\n", x, y, (x * y) );
         System.out.printf("x is %d and y is %d%n", x, y);
         System.out.printf("%d is not equal to %d\n", (x + y), (x * y) );
-        }        
+        }         
     }
