dc56a4ed152744da59f4f25cd5e4c9d0cb334e47
Topseen-a <abodunrintopseen@gmail.com>
2025-12-20 21:10:11 +0100

    Kata functions

---
+++ assignment/Kata.java
@@ -0,0 +1,15 @@
+public class Kata{
+    public static void main(String[] args){
+
+    System.out.println(isEven(10));
+}
+    
+    public static boolean isEven(int number){
+        if (number % 2 == 0){
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
+}
