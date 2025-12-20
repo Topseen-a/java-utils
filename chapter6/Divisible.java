9260be1cb1948ffb8af54cb5ee23450f5e1a59fb
Topseen-a <abodunrintopseen@gmail.com>
2025-12-20 20:23:31 +0100

    chapter 6

---
+++ chapter6/Divisible.java
@@ -0,0 +1,15 @@
+public class Divisible{
+    public static void main(String[] args){
+
+    System.out.println(isDivisible(11));
+    }
+
+    public static boolean isDivisible(int number){
+        if (number % 5 == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
