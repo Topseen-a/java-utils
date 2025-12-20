5529dd73cb2536fc452a2ef321a64d374b9e0000
Topseen-a <abodunrintopseen@gmail.com>
2025-12-20 20:15:43 +0100

    chapter 6

--- chapter6/Multiple.java
+++ chapter6/Multiple.java
@@ -1,29 +1,5 @@
-import java.util.Scanner;
 public class Multiple{
     public static void main(String[] args){
-
-    Scanner input = new Scanner(System.in);
-
-    while (true){
-        System.out.print("Enter the first number or -1 to exit: ");
-        int firstNumber = input.nextInt();
-
-        if (firstNumber == -1){
-            break;
-        }
-        else {
-            System.out.print("Enter second number: ");
-            int secondNumber = input.nextInt();
-        
-            if (isMultiple(firstNumber,secondNumber)){
-                System.out.println(secondNumber + "is a multiple of " + firstNumber);
-            }
-            else {
-                System.out.println(secondNumber + "is not a multiple of " + firstNumber);
-            }
-        }
-        
-    }
 
     System.out.println(isMultiple(4,12));
 }
