3e4bf32daa361b052320481de75958b551025b76
Topseen-a <abodunrintopseen@gmail.com>
2025-12-22 22:00:17 +0100

    Kata Functions

--- assignment/Kata.java
+++ assignment/Kata.java
@@ -1,7 +1,7 @@
 public class Kata{
     public static void main(String[] args){
 
-    System.out.println(getPositiveDifference(17, 12));
+    System.out.println(getDivision(24, 12));
 }
     
     public static boolean isEven(int number){
@@ -20,6 +20,14 @@ public class Kata{
         else {
             return secondNumber - firstNumber;
         }
+    }
 
+    public static float getDivision(int firstNumber, int secondNumber){
+        if(secondNumber == 0){
+            return 0;
+        }
+        else {
+            return firstNumber / secondNumber;
+        }
     }
 }
