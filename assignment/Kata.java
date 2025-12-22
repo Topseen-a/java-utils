735c740399ffe717c43f1bcccbedc155dce572e8
Topseen-a <abodunrintopseen@gmail.com>
2025-12-22 21:41:44 +0100

    kata functions

--- assignment/Kata.java
+++ assignment/Kata.java
@@ -1,7 +1,7 @@
 public class Kata{
     public static void main(String[] args){
 
-    System.out.println(isEven(10));
+    System.out.println(getPositiveDifference(17, 12));
 }
     
     public static boolean isEven(int number){
@@ -11,5 +11,15 @@ public class Kata{
         else {
             return false;
         }
+    }
+
+    public static int getPositiveDifference(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber){
+            return firstNumber - secondNumber;
+        }
+        else {
+            return secondNumber - firstNumber;
+        }
+
     }
 }
--- chapter6/Divisible.java
+++ chapter6/Divisible.java
@@ -1,7 +1,7 @@
 public class Divisible{
     public static void main(String[] args){
 
-    System.out.println(isDivisible(11));
+    System.out.println(isDivisible(10));
     }
 
     public static boolean isDivisible(int number){
