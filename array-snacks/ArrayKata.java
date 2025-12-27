9dbd808fc9e47f9581a9fb0b61cc0355861a47e2
Topseen-a <abodunrintopseen@gmail.com>
2025-12-27 21:13:55 +0100

    array snacks

--- array-snacks/ArrayKata.java
+++ array-snacks/ArrayKata.java
@@ -1,8 +1,8 @@
 public class ArrayKata{
     public static void main(String[] args){
 
-    int[] array = {1,2,8,4};
-    System.out.println(sumOfEvenNumbersIn(array));
+    int[] array = {1,3,8,4};
+    System.out.println(sumOfOddNumbersIn(array));
     }
 
     public static int maximumIn(int[] array){
@@ -35,8 +35,18 @@ public class ArrayKata{
 
     public static int sumOfEvenNumbersIn(int[] array){
         int sum = 0;
-        for (int count = 1; count < array.length; count++){
+        for (int count = 0; count < array.length; count++){
             if (array[count] % 2 == 0){
+                sum += array[count];
+            }
+        }
+        return sum;
+    }
+
+    public static int sumOfOddNumbersIn(int[] array){
+        int sum = 0;
+        for (int count = 0; count < array.length; count++){
+            if (array[count] % 2 != 0){
                 sum += array[count];
             }
         }
