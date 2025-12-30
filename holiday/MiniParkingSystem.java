02a0e183acd956a0ed261f84f4ed7770607ef7f4
Topseen-a <abodunrintopseen@gmail.com>
2025-12-30 20:30:28 +0100

    mini parking system

--- holiday/MiniParkingSystem.java
+++ holiday/MiniParkingSystem.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
 public class MiniParkingSystem{
     public static void main(String[] args){
 
-        
+    Scanner input = new Scanner(System.in);
+    
+        while(true){
+            System.out.println("Welcome to Mini Parking System. ");
+            System.out.println();
+            System.out.println("1. Choose a slot to park your car ");
+            System.out.println("2. Remove your car from the park ");
+            System.out.println("0. Exit the app ");
+            
+            System.out.print("Choose an option: ");
+            int choice = input.nextInt();
+        }
     }
 
 
