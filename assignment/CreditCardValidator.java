2603b0a299f47f80699ae09d4e7561e8d3ba351f
Topseen-a <abodunrintopseen@gmail.com>
2026-01-07 21:27:24 +0100

    Credit Card Validator

--- assignment/CreditCardValidator.java
+++ assignment/CreditCardValidator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class CreditCardValidator{
     public static void main(String[] args){
@@ -9,53 +10,5 @@ public class CreditCardValidator{
 
         
     }
-
-public static boolean isValidCard(String cardNumber){
-    int sum = 0;
-    
-    for (int index = cardNumber.length() - 1; index >= 0; index--){
-        
-    }
-}
-
-public static String cardType(String cardNumber){
-    String cardType;
-
-    if (cardNumber.startsWith("4")){
-        return "Visa Card";
-    }
-    else if (cardNumber.startsWith("5")){
-        return "Master Card";
-    }
-    else if (cardNumber.startsWith("37")){
-        return "American Express Card";
-    }
-    else if(cardNumber.startsWith("6")){
-        return "Discover Card";
-    }
-    else{
-        return "Unknown Card Type";
-    }
-}
-
-//public static int doubleSecondNumber(String cardNumber){
-//    int sum = 0;
-//
-//    for (int count = 1; count < cardNumber.length(); count += 2){
-//        int number = cardNumber
-//
-//
-//    }
-//
-//
-//
-//
-//
-//
-//}
-//
-
-
-
 
 }
