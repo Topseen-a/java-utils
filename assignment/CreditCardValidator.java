7f0234fa5c326a1664b1ae9684792e75f4dd77ba
Topseen-a <abodunrintopseen@gmail.com>
2026-01-07 22:54:03 +0100

    Credit Card Validator

--- assignment/CreditCardValidator.java
+++ assignment/CreditCardValidator.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class CreditCardValidator{
     public static void main(String[] args){
+
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter your card number: ");
