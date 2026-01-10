593c7ca2b0fc3497e3aa7cb131dcc73800457c25
Topseen-a <abodunrintopseen@gmail.com>
2026-01-10 22:23:13 +0100

    Student Grade

--- holiday/StudentGrade.java
+++ holiday/StudentGrade.java
@@ -5,6 +5,15 @@ public class StudentGrade{
     
         Scanner input = new Scanner(System.in);
 
+        System.out.print("How many student do you have? ");
+        int studentNumber = input.nextInt();
+
+        System.out.print("How many subjects do they offer? ");
+        int subjectNumber = input.nextInt();
+
+        System.out.println("\nSaving >>>>>>>>>>>>>>>>>>");
+        System.out.println("Saved succesfully");
+
     }
 
 }
