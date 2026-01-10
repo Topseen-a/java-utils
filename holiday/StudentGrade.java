35c1b0c49a47f2c374ce5ac4c337e415a5e77a10
Topseen-a <abodunrintopseen@gmail.com>
2026-01-10 22:18:01 +0100

    Student Grade

---
+++ holiday/StudentGrade.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class StudentGrade{
+    public static void main(String[] args){
+    
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
