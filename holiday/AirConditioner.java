7b79d98a0f256329329de1a4769c6716e49c080f
Topseen-a <abodunrintopseen@gmail.com>
2026-01-11 21:20:22 +0100

    Holiday

--- holiday/AirConditioner.java
+++ holiday/AirConditioner.java
@@ -1,4 +1,32 @@
 public class AirConditioner{
-    boolean isOn = false;
-    int temperature = 30;
+    boolean isOn;
+    int temperature = 24;
+    
+    public void turnOn(){
+        isOn = true;
+    }
+    
+    public void turnOff(){
+        isOn = false;
+    }
+
+    public boolean isOn(){
+        return isOn;    
+    }
+
+    public int getTemperature(){
+        return temperature;
+    }
+    
+    public void increaseTemp(){
+        if (temperature < 30){
+            temperature++;
+        }
+    }
+
+    public void decreaseTemp(){
+        if (temperature > 16){
+            temperature--;
+        }
+    }
 }
