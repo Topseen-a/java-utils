2bff4d55b0481408cd07230aebaf1dd7881773ef
Topseen-a <abodunrintopseen@gmail.com>
2026-01-11 11:59:07 +0100

    Student Grade

--- holiday/StudentGrade.java
+++ holiday/StudentGrade.java
@@ -15,14 +15,14 @@ public class StudentGrade{
 
         int[][] studentScores = new int[studentCount][subjectCount];
         int[] totalScoresPerStudent = new int[studentCount];
-        int[] averageScoresPerStudnt = new int[studentCount];
+        int[] averageScoresPerStudent = new int[studentCount];
         int[] studentPositions = new int[studentCount];
 
-//        int[] passedSubjects = new int[subjectNumber];
-//        int[] failedSubject = new int[subjectNumber];
+        int[] passPerSubject = new int[subjectCount];
+        int[] failPerSubject = new int[subjectCount];
 
-        int classTotalScores = 0;
-        int classAverageScores = 0;
+//        int classTotalScores = 0;
+//        int classAverageScores = 0;
 
         int highestScoreOverall = studentScores[0][0];
         int highestScoreStudent = 0;
@@ -53,20 +53,124 @@ public class StudentGrade{
                     highestScoreSubject = subjectIndex;
                 }
                 if (studentScores[studentIndex][subjectIndex] < lowestScoreOverall){
-                    lowestScoreStudent = studentScores[studentIndex][subjectIndex];
+                    lowestScoreOverall = studentScores[studentIndex][subjectIndex];
                     lowestScoreStudent = studentIndex;
-                    highestScoreSubject = subjectIndex; 
+                    lowestScoreSubject = subjectIndex; 
                 }
             }
             
             totalScoresPerStudent[studentIndex] = studentTotalScore;
-            averageScoresPerStudnt[studentIndex] = studentTotalScore / subjectCount;
+            averageScoresPerStudent[studentIndex] = studentTotalScore / subjectCount;
             
-            classTotalScores += studentTotalScore;
-            classAverageScores += averageScoresPerStudnt[studentIndex];
+//            classTotalScores += studentTotalScore;
+//            classAverageScores += averageScoresPerStudnt[studentIndex];
         }
 
-//        for (int studentIndex = 0; student < )
+        for (int studentIndex = 0; studentIndex < studentCount; studentIndex++){
+            int position = 1;
+
+            for (int otherStudentIndex = 0; otherStudentIndex < studentCount; otherStudentIndex++){
+                if (totalScoresPerStudent[otherStudentIndex] > totalScoresPerStudent[studentIndex]){
+                    position++;
+                }
+            }
+    
+            studentPositions[studentIndex] = position;
+        }
+
+        System.out.println("==================================================================");
+        System.out.printf("%-10s", "STUDENTS");
+
+        for (int subjectIndex = 1; subjectIndex <= subjectCount; subjectIndex++){
+            System.out.printf("%10s", "SUB" + subjectIndex);
+        }
+
+        System.out.printf("%10s%10s%10s%n", "TOT", "AVE", "POS");
+        System.out.println("==================================================================");
+
+        for (int studentIndex = 0; studentIndex < studentCount; studentIndex++){
+            System.out.printf("%-10s", "Student" + (studentIndex + 1));
+            for(int subjectIndex = 0; subjectIndex < subjectCount; subjectIndex++){
+                System.out.printf("%10d", studentScores[studentIndex][subjectIndex]);
+            }
+            System.out.printf("%10d", totalScoresPerStudent[studentIndex]);
+            System.out.printf("%10d", averageScoresPerStudent[studentIndex]);
+            System.out.printf("%10d", studentPositions[studentIndex]);
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("SUBJECT SUMMARY");
+        System.out.println("==================================================================");
+
+        for (int subjectIndex = 0; subjectIndex < subjectCount; subjectIndex++){
+            int highestScoreInSubject = studentScores[0][subjectIndex];
+            int lowestScoreInSubject = studentScores[0][subjectIndex];
+
+            int subjectTotalScore = 0;
+            double subjectAverageScore = 0;
+            
+            int passCount = 0;
+            int failCount = 0;
+
+            int highestScoringStudentInSubject = 0;
+            int lowestScoringStudentInSubject = 0;
+
+            System.out.println("Subject " + (subjectIndex + 1));
+
+            for (int studentIndex = 0; studentIndex < studentCount; studentIndex++){
+                int score = studentScores[studentIndex][subjectIndex];
+                subjectTotalScore += score;
+
+                if (score >= 50){
+                    passCount++;
+                }
+                else {
+                    failCount++;        
+                }
+
+                if (score > highestScoreInSubject){
+                    highestScoreInSubject = score;
+                    highestScoringStudentInSubject = studentIndex;
+                }
+                if (score < lowestScoreInSubject){
+                    lowestScoreInSubject = score;
+                    lowestScoringStudentInSubject = studentIndex;
+                }
+            }
+
+            subjectAverageScore = subjectTotalScore / studentCount;
+
+            System.out.println("Highest Scoring student is:  Student " + (highestScoringStudentInSubject + 1) + " scoring " + highestScoreInSubject);
+            System.out.println("Lowest Scoring student is:  Student " + (lowestScoringStudentInSubject + 1) + " scoring " + lowestScoreInSubject);
+            System.out.println("Total Score is: " + subjectTotalScore);
+            System.out.println("Average Score is: " + subjectAverageScore);
+            System.out.println("Number of Passes: " + passCount);
+            System.out.println("Number of Fails: " + failCount);
+            System.out.println();
+        }
+
+        int hardestSubjectIndex = 0;
+        int hardestSubjectFailCount = failPerSubject[0];
+        int easiestSubjectIndex = 0;
+        int easiestSubjectPassCount = passPerSubject[0];
+
+        for (int subjectIndex = 0; subjectIndex < subjectCount; subjectIndex++){
+            if (passPerSubject[subjectIndex] > easiestSubjectPassCount){
+                easiestSubjectIndex = subjectIndex;
+                easiestSubjectPassCount = passPerSubject[subjectIndex];
+            }
+            if (failPerSubject[subjectIndex] > hardestSubjectFailCount){
+                hardestSubjectIndex = subjectIndex;
+                hardestSubjectFailCount = failPerSubject[subjectIndex];
+            }
+        }
+        
+        System.out.println("\nThe hardest subject is subject " + (hardestSubjectIndex + 1) + " with " + hardestSubjectFailCount + " failures.");
+        System.out.println("The easiest subject is subject " + (easiestSubjectIndex + 1) + " with " + easiestSubjectPassCount + " passes.");
+        System.out.println("The overall highest score is scored by student " + (highestScoreStudent + 1) + " in subject " + (highestScoreSubject + 1) + " scoring " + highestScoreOverall);
+        System.out.println("The overall lowest score is scored by student " + (lowestScoreStudent + 1) + " in subject " + (lowestScoreSubject + 1) + " scoring " + lowestScoreOverall);
+        
     }
 
 }
