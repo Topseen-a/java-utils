67fd54ac6c651ee4027158a75a58c6624405a878
Topseen-a <abodunrintopseen@gmail.com>
2026-01-19 22:20:33 +0100

    MFDS

---
+++ java-test/LearningTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class LearningTest{
+
+    @Test
+    public void testThatMyCodeRuns(){
+        Assertions.assertEquals(1,1);
+    }
+}
