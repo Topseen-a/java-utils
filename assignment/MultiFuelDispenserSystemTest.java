9f18a766de75a7007089e31d225da4a2c65cf548
Topseen-a <abodunrintopseen@gmail.com>
2026-01-21 21:47:42 +0100

    MFDS

--- assignment/MultiFuelDispenserSystemTest.java
+++ assignment/MultiFuelDispenserSystemTest.java
@@ -2,13 +2,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MultiFuelDispenserSystemTest{
-
-    @Test
-    public void testGetBuyingMethod(){
-        
-
-    } 
-
     @Test
     public void testCalculateAmount(){
         double actual = MultiFuelDispenserSystem.calculateAmount(2,5);
@@ -21,5 +14,15 @@ public class MultiFuelDispenserSystemTest{
         double actual = MultiFuelDispenserSystem.calculateLitres(10,2);
         double expected = 5;
         Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testGetBuyingMethod(){
+        Assertions.assertNotNull(MultiFuelDispenserSystem.calculateAmount(4,5));
+    }
+
+    @Test
+    public void testValidAmountMethod(){
+        Assertions.assertNotNull(MultiFuelDispenserSystem.calculateLitres(7,8));
     }
 }
