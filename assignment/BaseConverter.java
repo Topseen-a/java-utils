67616f8867f92e268af7c640071df5ce39f964a0
Topseen-a <abodunrintopseen@gmail.com>
2026-01-25 09:36:49 +0100

    Base Converter

--- assignment/BaseConverter.java
+++ assignment/BaseConverter.java
@@ -1,1 +1,25 @@
+public class BaseConverter{
+    public static void main(String[] args){
 
+
+    }
+
+    public static int toBaseTwo(int number){
+
+        if (number < 0){
+            number *= -1;
+        }
+        else if (number == 0){
+            return "0";
+        }
+        else {
+            int division = 0;
+            int multiplication = 0;
+            int difference = 0;
+
+            
+
+        
+        }
+    }
+}
