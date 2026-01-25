7f37b5be36d44c2b42e382b1a06bbe17843365dc
Topseen-a <abodunrintopseen@gmail.com>
2026-01-25 11:03:52 +0100

    Base Converter

--- assignment/BaseConverter.java
+++ assignment/BaseConverter.java
@@ -2,6 +2,7 @@ public class BaseConverter{
     public static void main(String[] args){
 
         System.out.println(toBaseTwo(20));
+        System.out.println(toBaseFive(20));
     }
 
     public static String toBaseTwo(int number){
@@ -29,4 +30,31 @@ public class BaseConverter{
             return reverse;
         }
     }
+
+    public static String toBaseFive(int number){
+    
+        if (number < 0){
+            number *= -1;
+        }
+        if (number == 0){
+            return "0";
+        }
+        else {
+            String store = "";
+    
+            while (number > 0){
+                int remainder = number % 5;
+                store += remainder;
+                number /= 5;
+            }
+
+            String reverse = "";
+            for (int index = store.length() -1; index>= 0; index--){
+                reverse += store.charAt(index);
+            }
+            return reverse;
+        }
+    }
+
+    
 }
